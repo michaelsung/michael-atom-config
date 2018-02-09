194e52d062711251cdb967ff53a6f58529a10d29
Michael Sung <michael@Michaels-MBP.gateway>
2018-02-09 23:01:32 +1100

    initial commit

---
+++ atom-setup.sh
@@ -0,0 +1,16 @@
+# Packages
+apm install minimap
+apm install linter
+apm install linter-eslint
+apm install file-icons
+apm install color-picker
+apm install platformio-ide-terminal
+apm install autoclose-html
+apm install react
+apm install pretty-json
+apm install atom-clock
+apm install language-docker
+
+# Themes
+apm install nucleus-dark-ui
+apm install chester-atom-2-syntax
